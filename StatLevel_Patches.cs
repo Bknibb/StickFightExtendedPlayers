53ec95251a378afdb9859d6594d09f5a235c2d81
Bknibb <bailey.knibb@gmail.com>
2025-11-06 19:39:09 +1000

    Fix StatLevel not showing extra players

---
+++ StatLevel_Patches.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StickFightExtendedPlayers
+{
+    [HarmonyPatch(typeof(StatLevel), "PlayerIDToColor")]
+    public class StatLevel_Patches_PlayerIDToColor
+    {
+        static bool Prefix(int id, ref string __result)
+        {
+            if (id >= Plugin.NORMAL_PLAYERS)
+            {
+                __result = Plugin.NEW_COLOURS.Keys.ToList()[id - Plugin.NORMAL_PLAYERS].ToUpper();
+                return false;
+            }
+            return true;
+        }
+    }
+}
